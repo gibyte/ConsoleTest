72ae71e4b808ada68b71a38fb0a6fc759cedbc5d
Артур Стариков <Gibyte.mail@gmail.com>
2024-04-20 10:49:52 +0300

    мир25987

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("привет мир!!!!");
 Console.WriteLine("привет мир6");
-Console.WriteLine("привет мир2");
+Console.WriteLine("привет мир25987");
 Console.WriteLine("привет мир234");
